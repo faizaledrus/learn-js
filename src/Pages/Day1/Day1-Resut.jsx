1c49d778e4a4ad989f290e67965814609ef62274
faizaledrus <faizal.edrus@gmail.com>
2024-02-12 17:24:45 +0700

    update

---
+++ src/Pages/Day1/Day1-Resut.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Day1Resut() {
+
+  // tugas kamu adalah Memahami Layouting fungsing BOX, Container, Flex dkk
+  // buat sebuah halaman simpel yang mengartikan apa yang kamu pahami dari box / container / flext / stack / hstack / vstack
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Day1Resut
---
+++ src/Pages/Day1/Day1.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function Day1() {
+  return (
+    <Box bgColor='red'>
+    <Heading textAlign={'center'}>Andre</Heading>
+  </Box>
+  )
+}
+
+export default Day1
